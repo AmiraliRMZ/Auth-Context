d4dbf9ce112f5d6cca4fd7bbbd7d72be52cf6e4a
Amir RMZ <amir@example.com>
2025-12-06 17:49:26 +0330

    feat: add login page with fake API

---
+++ Login.tsx
@@ -0,0 +1,26 @@
+// Login.tsx
+import { useState } from "react";
+import { useAuth } from "./AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    await login(username);
+    navigate("/dashboard");
+  };
+
+  return (
+    <div>
+      <input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="username…"
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
