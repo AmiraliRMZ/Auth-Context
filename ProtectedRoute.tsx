e7f22a5542a3f4784b62c9a8be10ddc8ef4223db
Amir RMZ <amir@example.com>
2025-12-06 17:47:50 +0330

    feat: add ProtectedRoute component

---
+++ ProtectedRoute.tsx
@@ -0,0 +1,9 @@
+// ProtectedRoute.tsx
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
+import { ReactNode } from "react";
+
+export default function ProtectedRoute({ children }: { children: ReactNode }) {
+  const { user } = useAuth();
+  return user ? children : <Navigate to="/login" replace />;
+}
